8108afc413d12ff83caec12d53196be4d676b6a3
Jeiel Benedito <jeiel.benedito@gmail.com>
2024-03-25 21:47:22 -0400

    setup project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "./scss/index.scss";
+import "./i18n";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
+  </React.StrictMode>
+);
